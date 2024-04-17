6f656eb0c095bee919848bc5ebbb8d04df1ffdbb
Steven Hertz <hertzsteven@icloud.com>
2024-04-17 11:09:23 -0400

    no world

--- testNoUserInterface/ContentView.swift
+++ testNoUserInterface/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, to my world!")
+            Text("Hello, no world!")
         }
         .padding()
     }
