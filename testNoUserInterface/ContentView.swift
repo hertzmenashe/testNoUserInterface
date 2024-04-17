1ea068527c521fa110c0de7930448aa6b1a1e762
Steven Hertz <StevenH@appi4.com>
2024-04-17 11:05:00 -0400

    Initial Commit

---
+++ testNoUserInterface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testNoUserInterface
+//
+//  Created by Steven Hertz on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testNoUserInterface/testNoUserInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  testNoUserInterfaceApp.swift
+//  testNoUserInterface
+//
+//  Created by Steven Hertz on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct testNoUserInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
